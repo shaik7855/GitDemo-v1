168a566c83e66bfd5a45c28ef2b3f834b3d1274e
liyakath.s <liyakath.s@TL549.Technovert.in>
2025-01-07 11:53:22 +0530

    first commit

---
+++ src/test/Login.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Login {
+	
+	public static void main(String[] args) {
+	    
+	    int first = 10;
+	    int second = 20;
+
+	    // add two numbers
+	    int sum = first + second;
+	    System.out.println(first + " + " + second + " = "  + sum);
+
+	}
+}
